fff85783bcb5459a4a5984aa22cfa44b8c4b06aa
habib-wahid <habib11wahid@gmail.com>
2024-12-24 18:44:47 +0600

    keycloak authorization server flow added with react client

--- src/main/java/com/example/spring_security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/spring_security/config/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@ConditionalOnProperty(name = "security.jwt.enabled", havingValue = "true")
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final UserDetailsService userDetailsService;
--- src/main/java/com/example/spring_security/controller/RoleController.java
+++ src/main/java/com/example/spring_security/controller/RoleController.java
@@ -3,12 +3,14 @@ package com.example.spring_security.controller;
 import com.example.spring_security.service.RoleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "*")
 public class RoleController {
 
     private final RoleService roleService;
@@ -20,5 +22,11 @@ public class RoleController {
     @GetMapping("/roles")
     public ResponseEntity<?> getRoles() {
         return new ResponseEntity<>(roleService.getRoles(), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> greeting() {
+        return new ResponseEntity<>("Hello from spring", HttpStatus.OK);
     }
 }
